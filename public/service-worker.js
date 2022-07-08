c66aacc13dcde4a1948b89ae9e110d4208538d60
Chris Mullendore <chris@vindage.com>
2022-07-07 17:01:25 -0700

    WIP - Cache added

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,18 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
-    "./js/index.js"
+    "./js/index.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "./fonts/fontawesome-webfont.ttf",
+    "./fonts/fontawesome-webfont.woff",
+    "./fonts/fontawesome-webfont.woff2"
 ];
 
 
@@ -40,12 +51,5 @@ self.addEventListener('activate', function(e) {
       })
     );
   });
-/*
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-  
-    )
-  })
-  */
 
+  
